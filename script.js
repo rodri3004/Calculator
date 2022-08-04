aac020c888f4caeebe4da79f29168ac74b1dcf87
Rodrigo <rodri3004@hotmail.com>
2022-08-03 22:44:20 -0300

    finish calculator

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const displayValorAnteorior = document.getElementById('valor-anterior');
+const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
-const botonesNumeros = document.querySelectorAll('numero');
-const botonesOperadores = document.querySelectorAll('operador');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
